ed23ce030e968029a764d40ad8f8522cee3cea3d
Ryan Awad <ryanawad@yahoo.com>
2024-06-29 13:48:23 -0400

    fixed exit code bug in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,10 @@
 cp ./lib/libhnsw.so /usr/local/lib/ # copying shared library into /usr/local/lib for tests to run
 echo "Running tests..."
 go test ./... -count=1 # running the tests. "-count=1" stops test caching
+TEST_EXIT_CODE=$?
 
 # cleanup
 rm -rf /usr/local/lib/libhnsw.so
 echo "Done."
+
+exit $TEST_EXIT_CODE
