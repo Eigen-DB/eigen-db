98194123893174f00f16025c4444fedf30c3df09
Ryan Awad <ryanawad@yahoo.com>
2024-12-09 20:39:51 -0500

    fix: fixed bugs in benchmarking script

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -4,12 +4,39 @@ import requests
 import json
 import random
 import os
-from time import time
+import subprocess
+import psutil
+import pandas as pd
+from time import time, sleep
 
 EIGEN_ENDPOINT = "http://127.0.0.1:8080"
 API_KEY = "20417e0c21cea44e4b4fc90a06f57658" # change this to proper API key
 TEST_PARAMS = [
     {
+        "num_vectors": 500,
+        "num_trials": 100,
+        "k": 10,
+        "dim": 512,
+    },
+    {
+        "num_vectors": 500,
+        "num_trials": 100,
+        "k": 50,
+        "dim": 512,
+    },
+    {
+        "num_vectors": 500,
+        "num_trials": 100,
+        "k": 100,
+        "dim": 512,
+    },
+    {
+        "num_vectors": 500,
+        "num_trials": 100,
+        "k": 500,
+        "dim": 512,
+    },
+    {
         "num_vectors": 1_000,
         "num_trials": 100,
         "k": 10,
@@ -25,6 +52,36 @@ TEST_PARAMS = [
         "num_vectors": 1_000,
         "num_trials": 100,
         "k": 100,
+        "dim": 512,
+    },
+    {
+        "num_vectors": 1_000,
+        "num_trials": 100,
+        "k": 500,
+        "dim": 512,
+    },
+    {
+        "num_vectors": 5_000,
+        "num_trials": 100,
+        "k": 10,
+        "dim": 512,
+    },
+    {
+        "num_vectors": 5_000,
+        "num_trials": 100,
+        "k": 50,
+        "dim": 512,
+    },
+    {
+        "num_vectors": 5_000,
+        "num_trials": 100,
+        "k": 100,
+        "dim": 512,
+    },
+    {
+        "num_vectors": 5_000,
+        "num_trials": 100,
+        "k": 500,
         "dim": 512,
     },
     {
@@ -46,21 +103,9 @@ TEST_PARAMS = [
         "dim": 512,
     },
     {
-        "num_vectors": 100_000,
+        "num_vectors": 10_000,
         "num_trials": 100,
-        "k": 10,
-        "dim": 512,
-    },
-    {
-        "num_vectors": 100_000,
-        "num_trials": 100,
-        "k": 50,
-        "dim": 512,
-    },
-    {
-        "num_vectors": 100_000,
-        "num_trials": 100,
-        "k": 100,
+        "k": 500,
         "dim": 512,
     },
 ]
@@ -70,10 +115,11 @@ TEST_PARAMS = [
 def create_random_vector(dim: int) -> "list[float]":
     v = []
     for i in range(dim):
-        v.append(random.uniform(0.0, 200.0))
+        v.append(random.uniform(-200.0, 200.0))
     return v
 
-def setup(num_vectors: int, dim: int) -> None:
+# reuse_req_body allows multiple setups with the same num_vectors to use the first setup's randomly generated vectors instead of re-computing them.
+def setup(num_vectors: int, dim: int, reuse_req_body: dict = None) -> None:
     req_body = {"vectors": []}
     for i in range(num_vectors):
         req_body["vectors"].append({
@@ -90,15 +136,42 @@ def setup(num_vectors: int, dim: int) -> None:
     )
 
     if req.status_code != 200 or json.loads(req.content.decode()).get("message") != f"{num_vectors}/{num_vectors} vectors successfully inserted.":
-        print(f"ERROR: request to /vector/bulk-insert failed. Response: {req.content.decode()}")
-        exit(1)
+        print(f"SETUP ERROR: request to /vector/bulk-insert failed. Response: {req.content.decode()}")
 
-def cleanup() -> None:
+def start_eigen_db() -> None:
+    log_stream = open(f'./eigen_db-{time()}.log', mode='a')
+    subprocess.Popen(
+        args=['./eigen_db'],
+        cwd='../',
+        stdout=log_stream,
+        stderr=log_stream
+    )
+
+def find_and_kill_process(binary_name: str) -> None:
+    for process in psutil.process_iter(['pid', 'name']):
+        try:
+            if binary_name == process.info['name']:
+                print(f"Killing process {binary_name} with PID {process.info['pid']}")
+                process.kill()
+                return
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    print(f"No running process found with name \"{binary_name}\"")
+
+def cleanup(restart=True) -> None:
+    find_and_kill_process('eigen_db')
+    sleep(1) # ensure the process is truly killed
+
     if os.path.exists("../eigen/hnsw_index.bin"):
         os.remove("../eigen/hnsw_index.bin")  
 
     if os.path.exists("../eigen/vector_space.vec"):
         os.remove("../eigen/vector_space.vec")    
+
+    if restart:
+        print('Restarting EigenDB...')
+        start_eigen_db()
+
 
 ####### BENCHMARKING FUNCTIONS ####### 
 
@@ -166,34 +239,51 @@ def benchmark_inserting(num_vectors: int, dim: int, num_trials: int) -> float:
 
 
 if __name__ == "__main__":
-    param = {
-        "num_vectors": 1_000,
-        "num_trials": 100,
-        "k": 10,
-        "dim": 512,
-    }
+    print('WARNING: running this script will delete all existing persisted data. This script it mean to be ran for development purposes.')
+    answer = input('Are you sure you want proceed? (y/N): ')
+    if answer != 'y':
+        print('Exitting...')
+        exit(0)
 
-    print(f"Metrics for the following params:\n{json.dumps(param, indent=4)}")
-
-    setup(
-        num_vectors=param["num_vectors"], 
-        dim=param["dim"]
+    index_df = pd.DataFrame(
+        columns=['num_vectors', 'k', 'dim', 'mean_time_secs']
     )
 
-    index_mean = benchmark_indexing(
-        num_vectors=param["num_vectors"],
-        num_trials=param["num_trials"], 
-        k=param["k"]
+    insert_df = pd.DataFrame(
+        columns=['num_vectors', 'k', 'dim', 'mean_time_secs']
     )
-    print(f"index mean: {index_mean}s")
 
-    insert_mean = benchmark_inserting(
-        num_vectors=param["num_vectors"],
-        num_trials=param["num_trials"],
-        dim=param["dim"]
-    )
-    print(f"insert mean: {insert_mean}s")
+    cleanup(restart=False)
+    start_eigen_db()
 
-    cleanup()
+    for param in TEST_PARAMS:
+        print(f"Metrics for the following params:\n{json.dumps(param, indent=4)}")
 
+        setup(
+            num_vectors=param["num_vectors"], 
+            dim=param["dim"]
+        )
+
+        index_mean = benchmark_indexing(
+            num_vectors=param["num_vectors"],
+            num_trials=param["num_trials"], 
+            k=param["k"]
+        )
+        index_df.loc[len(index_df)] = [param['num_vectors'], param['k'], param['dim'], index_mean] # append row in the df
+        print(f"index mean: {index_mean}s")
+
+        insert_mean = benchmark_inserting(
+            num_vectors=param["num_vectors"],
+            num_trials=param["num_trials"],
+            dim=param["dim"]
+        )
+        insert_df.loc[len(insert_df)] = [param['num_vectors'], param['k'], param['dim'], insert_mean] # append row in the df
+        print(f"insert mean: {insert_mean}s")
+
+        cleanup()
+    
+    index_df.to_csv(f'./indexing_mean_{time()}.csv', index=False)
+    insert_df.to_csv(f'./inserting_mean_{time()}.csv', index=False)
+
+    cleanup(restart=False)
     exit(0)
