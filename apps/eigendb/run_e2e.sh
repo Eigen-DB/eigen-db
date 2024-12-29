7b1c09e72958c45c0e3c3fbfeb89d76cecb73014
Ryan Awad <ryanawad@yahoo.com>
2024-12-29 17:53:18 -0500

    ci: downloading venom binary if not installed in runner when running e2e for eigendb

--- apps/eigendb/run_e2e.sh
+++ apps/eigendb/run_e2e.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 docker compose -f docker-compose-test.yml up -d --build
 
-venom run e2e/ --output-dir=e2e/logs
+venom version
+if $? -ne 0; then
+    echo "Venom is not installed. Installing venom binary."
+    curl https://github.com/ovh/venom/releases/download/v1.1.0/venom.linux-amd64 -L -o ./venom && chmod +x ./venom
+    VENOM=./venom
+else
+    echo "Venom is already installed."
+    VENOM=$(which venom)
+fi
+
+$VENOM run e2e/ --output-dir=e2e/logs
 E2E_TEST_EXIT_CODE=$?
 
 docker compose down
