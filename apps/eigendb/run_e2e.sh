8893ba23f2c707e1f9314ad7e2fd2c1a52125884
Ryan Awad <ryanawad@yahoo.com>
2024-12-30 09:58:51 -0500

    ci(eigendb): fixed syntax error in run_e2e.sh

--- apps/eigendb/run_e2e.sh
+++ apps/eigendb/run_e2e.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker compose -f docker-compose-test.yml up -d --build
 
-venom version
-if $? -ne 0; then
+which venom
+if [[ $? -ne 0 ]]; then
     echo "Venom is not installed. Installing venom binary."
     curl https://github.com/ovh/venom/releases/download/v1.1.0/venom.linux-amd64 -L -o ./venom && chmod +x ./venom
     VENOM=./venom
